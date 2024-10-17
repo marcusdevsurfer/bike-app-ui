618389bf4cd2ce32ab28ee4bc22cb7cbe288a6cc
marcusdevsurfer <marcus.sea13@gmail.com>
2024-10-17 16:14:55 -0600

    refactoring

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, SafeAreaView } from 'react-native';
-import { BikesView } from './components/BikesView';
+import { AppNavigator } from './AppNavigator';
+
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-      <BikesView />
+      <AppNavigator />
     </SafeAreaView>
   );
 }
