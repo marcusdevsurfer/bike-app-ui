17cdf4680e901ea046b882c10e707264929ac4f0
marcusdevsurfer <marcus.sea13@gmail.com>
2024-10-17 17:00:42 -0600

    add: bike form initialization

---
+++ components/AddBikeFom.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+
+export const AddBikeForm = () => {
+    return(
+        <View>
+            <TextInput placeholder='Field 1'>
+
+            </TextInput>
+
+            <TextInput placeholder='Field 1'>
+
+            </TextInput>
+
+            <TextInput placeholder='Field 1'>
+
+            </TextInput>
+
+            <TextInput placeholder='Field 1'>
+
+            </TextInput>
+
+        </View>
+
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+})
