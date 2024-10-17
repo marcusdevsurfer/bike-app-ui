0435253e19c2b456c87292c4579c280f8b7b0477
marcusdevsurfer <marcus.sea13@gmail.com>
2024-10-17 16:15:17 -0600

    add: addition of home view

---
+++ components/Home.js
@@ -0,0 +1,33 @@
+import { Text, SafeAreaView, Pressable, StyleSheet } from "react-native"
+export const Home = ({ navigation }) => {
+    return (
+        <SafeAreaView style={styles.container}>
+            <Pressable style={styles.button} onPress={() => navigation.navigate('BikesView')}>
+                <Text style={styles.text}>Administrar bicicletas</Text>
+            </Pressable>
+            <Pressable style={styles.button} onPress={() => navigation.navigate('BikesAppointmentsView')}>
+                <Text style={styles.text}>Citas</Text>
+            </Pressable>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        flexWrap: 'wrap',
+    },
+    button: {
+        flexGrow: 1,
+        backgroundColor: 'black',
+        padding: 30,
+        margin: 10,
+        borderRadius: '20px'
+    },
+    text: {
+        color: 'white',
+        fontSize: 22
+    },
+});
