e1f3d97e96e8ee859ed6c05cf1429d6c00fe8234
marcusdevsurfer <marcus.sea13@gmail.com>
2024-10-17 13:22:19 -0600

    add: bike action component

---
+++ components/BikesActions.js
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react"
+import { Text, StyleSheet, Pressable, TextInput, View } from "react-native"
+
+export const BikesActions = () => {
+    const [text, setText] = useState('')
+
+    useEffect(() => {
+        console.log(text)
+    }, [text])
+
+    return (
+        <View style={styles.sectionContainer}>
+            <TextInput
+                style={styles.input}
+                onChangeText={setText}
+                value={text}
+                placeholder={"Buscar"}
+            />
+            <Pressable style={styles.button}>
+                <Text style={styles.text}>Agregar</Text>
+            </Pressable>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    sectionContainer: {
+        margin: 10,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: "space-between"
+    },
+    input: {
+        height: 40,
+        width: 180,
+        padding: 10,
+        borderWidth: 1,
+        borderRadius: '4px'
+    },
+    button: {
+        padding: 8,
+        backgroundColor: 'black',
+        borderRadius: '4px'
+    },
+    text: {
+        color: 'white'
+    },
+})
