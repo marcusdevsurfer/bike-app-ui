1092bd227193d1ef2a84a07205167be4babb1e99
marcusdevsurfer <marcus.sea13@gmail.com>
2024-10-17 13:23:38 -0600

    refact: app refactored with new components

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, SafeAreaView } from 'react-native';
+import { BikesView } from './components/BikesView';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      <BikesView />
+    </SafeAreaView>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
 });
