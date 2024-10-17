bd4a03672e1d180e433e2ed06e7d311f64d09552
marcusdevsurfer <marcus.sea13@gmail.com>
2024-10-17 13:23:09 -0600

    add: bikes table information component

---
+++ components/BikesTable.js
@@ -0,0 +1,54 @@
+import { FlatList, View } from 'react-native';
+import { BikeItem } from './BikeItem';
+
+
+const bicicles = [
+    {
+        id: '1',
+        serialNumber: 'YGYU22',
+        model: "2020",
+        status: "available",
+        station: {
+            id: "1",
+            name: "Soriana"
+        }
+    },
+    {
+        id: '2',
+        serialNumber: 'IJUHT5',
+        model: "2019",
+        status: "available",
+        station: {
+            id: "2",
+            name: "Capdam"
+        }
+    },
+    {
+        id: '3',
+        serialNumber: '7678HG',
+        model: "2024",
+        status: "in repair",
+        station: {
+            id: "3",
+            name: "Brisas"
+        }
+    }
+];
+
+export const BikesTable = () => {
+    return (
+        <View>
+            <FlatList
+                data={bicicles}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item }) => (
+                    <BikeItem bike={item} />
+                )}
+            />
+        </View>
+    )
+}
+
+
+
+
