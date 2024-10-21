e0af9b21532fc36ad1892bec774240f16146458c
marcusdevsurfer <marcus.sea13@gmail.com>
2024-10-21 11:45:49 -0600

    refact: id changed

--- components/AppointmentsView.js
+++ components/AppointmentsView.js
@@ -11,12 +11,11 @@ const fetchAppointments = () => {
                 { id: '1', bikeSerial: '1234AB', date: '2024-10-22', time: '10:00 AM', station: 'Soriana' },
                 { id: '2', bikeSerial: '5678CD', date: '2024-10-21', time: '11:00 AM', station: 'Capdam' },
                 { id: '3', bikeSerial: '7678HG', date: '2024-10-21', time: '12:00 PM', station: 'Brisas' },
-                { id: '3', bikeSerial: '7678HG', date: '2024-10-21', time: '12:00 PM', station: 'Brisas' },
-
-                { id: '3', bikeSerial: '7678HG', date: '2024-10-21', time: '12:00 PM', station: 'Brisas' },
-                { id: '3', bikeSerial: '7678HG', date: '2024-10-21', time: '12:00 PM', station: 'Brisas' },
-                { id: '3', bikeSerial: '7678HG', date: '2024-10-21', time: '12:00 PM', station: 'Brisas' },
-                { id: '3', bikeSerial: '7678HG', date: '2024-10-21', time: '12:00 PM', station: 'Brisas' },
+                { id: '4', bikeSerial: '7678HG', date: '2024-10-21', time: '12:00 PM', station: 'Brisas' },
+                { id: '5', bikeSerial: '7678HG', date: '2024-10-21', time: '12:00 PM', station: 'Brisas' },
+                { id: '6', bikeSerial: '7678HG', date: '2024-10-21', time: '12:00 PM', station: 'Brisas' },
+                { id: '7', bikeSerial: '7678HG', date: '2024-10-21', time: '12:00 PM', station: 'Brisas' },
+                { id: '8', bikeSerial: '7678HG', date: '2024-10-21', time: '12:00 PM', station: 'Brisas' },
             ]);
         }, 2000);
     });
