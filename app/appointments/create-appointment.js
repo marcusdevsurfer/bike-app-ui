7599c8963890fec2410fce1ac8b2341c5e6541d2
marcusdevsurfer <marcus.sea13@gmail.com>
2024-10-22 23:15:08 -0600

    add: create appointment route

---
+++ app/appointments/create-appointment.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { CreateAppointmentView } from '../components/CreateAppointmentView'
+
+export default function CreateAppointment() {
+    return (
+        <CreateAppointmentView />
+    )
+}
+
