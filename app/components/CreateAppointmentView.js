bcedb1468e0f7b32cd1d205b58877afffee7f4fd
marcusdevsurfer <marcus.sea13@gmail.com>
2024-10-22 23:31:16 -0600

    refact: addition of new input and button to send

--- app/components/CreateAppointmentView.js
+++ app/components/CreateAppointmentView.js
@@ -1,20 +1,18 @@
 import React from 'react'
-import { Text, View, TextInput, StyleSheet } from 'react-native'
+import { Text, View, TextInput, StyleSheet, Pressable } from 'react-native'
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { globalStyles } from '../../styles/globalStyles';
 import { HeaderNavigation } from './HeaderNavigation';
 
 export const CreateAppointmentView = () => {
     const insets = useSafeAreaInsets();
-
     return (
         <View style={[globalStyles.container, { paddingTop: insets.top, paddingBottom: insets.bottom }]}>
             <HeaderNavigation />
             <View style={styles.sectionContainer}>
-                <Text style={globalStyles.title}>Crear</Text>
+                <Text style={globalStyles.title}>Crear reservacion.</Text>
                 <Text style={globalStyles.subtitle}>Agenda una nueva cita y disfruta del servicio totalmente gratuito.</Text>
             </View>
-
             <View style={styles.formContainer}>
                 {/* Name input */}
                 <Text style={globalStyles.text}>Ingresa tu nombre</Text>
@@ -22,6 +20,13 @@ export const CreateAppointmentView = () => {
                 {/* Date input */}
                 <Text style={globalStyles.text}>Selecciona la fecha y hora de tu cita</Text>
                 <TextInput style={globalStyles.input} placeholder="ej. Dia / Mes / Año" />
+                {/* Bike seleccion input */}
+                <Text style={globalStyles.text}>Selecciona tu bicicleta</Text>
+                <TextInput style={globalStyles.input} placeholder="ej. Bicleta" />
+
+                <Pressable style={globalStyles.button}>
+                    <Text style={globalStyles.buttonText}>Crear</Text>
+                </Pressable>
             </View>
         </View>
     )
@@ -29,9 +34,13 @@ export const CreateAppointmentView = () => {
 
 const styles = StyleSheet.create({
     sectionContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
         padding: 20,
     },
     formContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
         padding: 20,
     },
 });
