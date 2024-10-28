043e4fe0b999e4e4714003a9dbc1ed19602032d5
marcusdevsurfer <marcus.sea13@gmail.com>
2024-10-28 11:11:53 -0600

    add: users view

---
+++ app/components/UsersView.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const UsersView = () => {
+  return (
+    <View>
+      <Text>
+        UsersView
+      </Text>
+    </View>
+  )
+}
