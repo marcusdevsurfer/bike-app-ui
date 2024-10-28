dc655fb5c6d400b20e2863f4dda1fb65add14612
marcusdevsurfer <marcus.sea13@gmail.com>
2024-10-28 11:42:27 -0600

    refact: header container addition

--- app/components/UsersView.js
+++ app/components/UsersView.js
@@ -1,12 +1,24 @@
 import React from 'react'
-import { Text, View } from 'react-native'
-
+import { Text, View, StyleSheet } from 'react-native'
+import { HeaderNavigation } from './HeaderNavigation'
+import { getInsets, globalStyles } from '../../styles/globalStyles'
 export const UsersView = () => {
   return (
-    <View>
-      <Text>
-        UsersView
-      </Text>
+    <View style={[getInsets(), globalStyles.container]}>
+      <HeaderNavigation />
+      <View style={styles.headerContainer}>
+        <Text style={globalStyles.title}>Usuarios Registrados</Text>
+        <Text style={globalStyles.subtitle}>Aqui se muestran los usuarios registrados en la aplicacion.</Text>
+      </View>
+      <View>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
