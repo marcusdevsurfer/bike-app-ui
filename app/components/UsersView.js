4195d90c8ac9ae844869357a31dfabf589481f63
marcusdevsurfer <marcus.sea13@gmail.com>
2024-10-28 13:10:37 -0600

    refact: input to search users

--- app/components/UsersView.js
+++ app/components/UsersView.js
@@ -1,8 +1,9 @@
-import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { Text, View, StyleSheet, TextInput } from 'react-native'
 import { HeaderNavigation } from './HeaderNavigation'
 import { getInsets, globalStyles } from '../../styles/globalStyles'
 export const UsersView = () => {
+  const [input, setInput] = useState('')
   return (
     <View style={[getInsets(), globalStyles.container]}>
       <HeaderNavigation />
@@ -10,8 +11,15 @@ export const UsersView = () => {
         <Text style={globalStyles.title}>Usuarios Registrados</Text>
         <Text style={globalStyles.subtitle}>Aqui se muestran los usuarios registrados en la aplicacion.</Text>
       </View>
-      <View>
+      <View style={styles.findBarContainer}>
+        <TextInput
+          style={globalStyles.input}
+          onChangeText={setInput}
+          value={input}
+          placeholder='Buscar usuario'
+        />
       </View>
+      <Text>{input}</Text>
     </View>
   )
 }
@@ -20,5 +28,10 @@ const styles = StyleSheet.create({
   headerContainer: {
     justifyContent: 'center',
     alignItems: 'center',
-  }
+  },
+  findBarContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+  },
 })
