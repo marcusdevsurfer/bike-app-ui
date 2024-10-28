24c38870643bf6671d615205dcd6c48d6d56cddb
marcusdevsurfer <marcus.sea13@gmail.com>
2024-10-28 13:28:17 -0600

    refact: flat list addition (in progress)

--- app/components/UsersView.js
+++ app/components/UsersView.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Text, View, StyleSheet, TextInput } from 'react-native'
+import { Text, View, StyleSheet, TextInput, FlatList } from 'react-native'
 import { HeaderNavigation } from './HeaderNavigation'
 import { getInsets, globalStyles } from '../../styles/globalStyles'
 import { MaterialIcons } from '@expo/vector-icons';
@@ -9,11 +9,12 @@ export const UsersView = () => {
   return (
     <View style={[getInsets(), globalStyles.container]}>
       <HeaderNavigation />
+      {/* Header */}
       <View style={styles.headerContainer}>
         <Text style={globalStyles.title}>Usuarios Registrados</Text>
         <Text style={globalStyles.subtitle}>Aqui se muestran los usuarios registrados en la aplicacion.</Text>
       </View>
-
+      {/* Find Bar */}
       <View style={styles.findBarContainer}>
         <MaterialIcons name="search" size={24} color="#666" style={styles.searchIcon} />
         <TextInput
@@ -23,7 +24,13 @@ export const UsersView = () => {
           placeholder='Buscar usuario'
         />
       </View>
-      <Text>{input}</Text>
+      {/* Items (Users) */}
+      <FlatList
+        data={[
+          { key: '1', name: 'Juan', email: 'Perez', password: '',role: 'admin', phone: '', membershipStatus: 'active', createdAt: '' },]}
+        renderItem={({ item }) => (<Text>{item.name} {item.lastName}</Text>)}
+        contentContainerStyle={{ flex: 1, backgroundColor : '', margin: 10, padding: 10 }}
+      />
     </View>
   )
 }
