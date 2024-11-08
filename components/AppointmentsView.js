064f6ab45d7e98bfe9b0ff1ff713d8f85db786c0
marcusdevsurfer <marcus.sea13@gmail.com>
2024-11-08 13:50:16 -0600

    refact: code clean and api connection

--- components/AppointmentsView.js
+++ components/AppointmentsView.js
@@ -1,136 +1,69 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, FlatList, ActivityIndicator, Platform, TouchableOpacity } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
-import PropTypes from 'prop-types';
+import { View, Text, StyleSheet, FlatList, ActivityIndicator, Platform } from "react-native";
 import { MaterialIcons } from '@expo/vector-icons';
-import { useRouter } from 'expo-router';
-import { fetchAppointments } from "../app/repository/appointmentRepository";
+import { API_URL } from '@env';
+import { HeaderNavigation } from "../app/components/HeaderNavigation";
+import { globalStyles, getInsets } from "../styles/globalStyles";
 
-const formatDate = (dateString) => {
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    return new Date(dateString).toLocaleDateString(undefined, options);
-};
-
-const getTodayAppointments = (appointments) => {
-    const today = new Date().toISOString().split('T')[0];
-    console.log('Today:  ' + today);
-    return appointments.filter(appointment => {
-        console.log(appointment.date);
-        return appointment.date === today;
-    });
-};
 
 const AppointmentItem = ({ appointment }) => (
     <View style={styles.itemContainer}>
         <View style={styles.itemHeader}>
             <MaterialIcons name="directions-bike" size={24} color="#007bff" />
-            <Text style={styles.itemTitle}>Serial: {appointment.bikeSerial}</Text>
+            <Text style={styles.itemTitle}>Serial:</Text>
         </View>
-        <Text style={styles.itemText}><MaterialIcons name="event" size={16} color="#007bff" /> Fecha: {appointment.date}</Text>
-        <Text style={styles.itemText}><MaterialIcons name="schedule" size={16} color="#007bff" /> Hora: {appointment.time}</Text>
-        <Text style={styles.itemText}><MaterialIcons name="location-on" size={16} color="#007bff" /> Estación: {appointment.station}</Text>
+        <Text style={styles.itemText}><MaterialIcons name="event" size={16} color="#007bff" /> Fecha: {new Date(appointment?.rentalStartTime).toLocaleDateString()} </Text>
+        <Text style={styles.itemText}><MaterialIcons name="schedule" size={16} color="#007bff" /> Hora: {new Date(appointment?.rentalStartTime).toLocaleTimeString()}</Text>
+        <Text style={styles.itemText}><MaterialIcons name="location-on" size={16} color="#007bff" /> Estación: {appointment?.stationStart}</Text>
     </View>
 );
 
 export default function AppointmentsView() {
-    const insets = useSafeAreaInsets();
-    const router = useRouter();
-    const [appointments, setAppointments] = useState([]);
+    const [rentals, setRentals] = useState([]);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        fetchAppointments().then(data => {
-            setAppointments(getTodayAppointments(data));
-            setLoading(false);
-        });
+        fetchRentals();
     }, []);
 
-    const today = new Date().toISOString().split('T')[0];
-    const formattedToday = formatDate(today);
+    const fetchRentals = async () => {
+        try {
+            const response = await fetch(`${API_URL}/rentals`);
+            const data = await response.json();
+            setRentals(data);
+            setLoading(false);
+        } catch (error) { console.log(error); }
 
+    }
 
     return (
-        <View style={[styles.container, { paddingTop: insets.top, paddingBottom: insets.bottom }]}>
-            <View style={styles.header}>
-                <TouchableOpacity onPress={() => router.push('/')} style={styles.backButton}>
-                    <MaterialIcons name="arrow-back" size={24} color="#007bff" />
-                    <Text style={styles.backButtonText}>Inicio</Text>
-                </TouchableOpacity>
-                <Text style={styles.bikeCount}>Bicicletas: {appointments.length}</Text>
-            </View>
-            <Text style={styles.title}>Citas Registradas</Text>
-            <Text style={styles.subtitle}>Hoy es {formattedToday}. Mostrando citas para hoy.</Text>
+        <View style={[globalStyles.container, getInsets()]}>
+            <HeaderNavigation />
+            <Text style={globalStyles.title}>Citas Registradas</Text>
             {
                 loading ? <ActivityIndicator size="large" color="#007bff" /> :
-                    appointments.length > 0 ? (
+                    rentals.length > 0 ? (
                         <FlatList
-                            data={appointments}
+                            data={rentals}
+                            keyExtractor={item => item._id}
                             renderItem={({ item }) => <AppointmentItem appointment={item} />}
-                            keyExtractor={item => item.id}
-                            horizontal={Platform.OS === 'web'}
                             contentContainerStyle={styles.listContent}
                         />
                     ) : (
-                        <Text style={styles.noAppointmentsText}>No hay citas para hoy.</Text>
+                        <Text style={globalStyles.subtitle}>No hay citas para hoy.</Text>
                     )}
         </View>
     );
 }
 
-AppointmentItem.propTypes = {
-    appointment: PropTypes.shape({
-        bikeSerial: PropTypes.string.isRequired,
-        date: PropTypes.string.isRequired,
-        time: PropTypes.string.isRequired,
-        station: PropTypes.string.isRequired,
-    }).isRequired,
-};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        padding: 20,
-        backgroundColor: '#f8f9fa',
-    },
-    header: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        width: '100%',
-        marginVertical: 20,
-    },
-    backButton: {
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    backButtonText: {
-        fontSize: 18,
-        color: '#007bff',
-        marginLeft: 5,
-    },
     bikeCount: {
         fontSize: 18,
         color: '#007bff',
     },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginBottom: 10,
-    },
-    subtitle: {
-        fontSize: 18,
-        color: '#666',
-        marginBottom: 20,
-    },
-    listContent: {
-        width: '100%',
-        flexDirection: Platform.OS === 'web' ? 'row' : 'column',
-        justifyContent: 'center',
-        alignItems: 'flex-start',
-    },
     itemContainer: {
-        padding: 30,
+        padding: 20,
         margin: 10,
         backgroundColor: '#fff',
         borderRadius: 10,
@@ -139,6 +72,11 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.1,
         shadowRadius: 4,
         elevation: 3,
+    },
+    listContent: {
+        flexDirection: Platform.OS === 'web' ? 'row' : 'column',
+        flexWrap: Platform.OS === 'web' ? 'wrap' : 'no-wrap',
+        justifyContent: 'center',
     },
     itemHeader: {
         flexDirection: 'row',
@@ -153,17 +91,5 @@ const styles = StyleSheet.create({
     itemText: {
         fontSize: 16,
         marginVertical: 2,
-    },
-    loadingText: {
-        fontSize: 18,
-        color: '#666',
-        textAlign: 'center',
-        marginTop: 20,
-    },
-    noAppointmentsText: {
-        fontSize: 18,
-        color: '#666',
-        textAlign: 'center',
-        marginTop: 20,
     },
 });
