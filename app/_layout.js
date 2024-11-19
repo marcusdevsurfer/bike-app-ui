b9ba60ce17d1f32281041344b2d807536dd3fa4d
marcusdevsurfer <marcus.sea13@gmail.com>
2024-11-18 20:29:20 -0600

    code refact

--- app/_layout.js
+++ app/_layout.js
@@ -1,27 +1,8 @@
 import React from "react";
-import { useEffect } from "react";
-import { useRouter } from "expo-router";
 import { Slot } from "expo-router";
 import { View, StyleSheet } from "react-native";
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Layout() {
-    const router = useRouter();
-
-    useEffect(() => {
-        const getSession = async () => {
-            try {
-                const session = await AsyncStorage.getItem('@session_key');
-                if (!session) {
-                    router.push('/auth');
-                }
-            } catch (e) {
-                console.error('Failed to get session.', e);
-            }
-        };
-        getSession();
-    }, []);
-
     return (
         <View style={styles.container}>
             <Slot />
