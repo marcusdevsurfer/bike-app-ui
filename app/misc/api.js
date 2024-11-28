390d71b3182083926f6af042df281c792cccef08
marcusdevsurfer <marcus.sea13@gmail.com>
2024-11-28 13:45:12 -0600

    new api calls method added

--- app/misc/api.js
+++ app/misc/api.js
@@ -1,5 +1,46 @@
 import { API_URL } from '@env';
 
+export const fetchUser = async (id) => {
+    try {
+        const response = await fetch(`${API_URL}/users/${id}`);
+        const json = await response.json();
+        return json;
+    } catch (error) {
+        console.error(error + "Error en fetchUser");
+    }
+};
+
+export const fetchRental = async (id) => {
+    try {
+        const response = await fetch(`${API_URL}/rentals/${id}`);
+        const json = await response.json();
+        return json;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export const fetchBike = async (id) => {
+    try {
+        const response = await fetch(`${API_URL}/bikes/${id}`);
+        const json = await response.json();
+        return json;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const fetchStation = async (id) => {
+    try {
+        const response = await fetch(`${API_URL}/stations/${id}`);
+        const json = await response.json();
+        return json;
+    } catch (error) {
+        console.error(error);
+    }
+
+};
+
 export const fetchAndSetStations = async (setData) => {
     try {
         const response = await fetch(`${API_URL}/stations`)
